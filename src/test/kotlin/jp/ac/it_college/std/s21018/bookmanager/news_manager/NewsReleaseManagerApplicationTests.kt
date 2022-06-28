b66f6c95b028689f71770ae9fd7a2941671c5670
s21018 <s21018@std.it-cllege.ac.jp>
2022-06-28 15:51:43 +0900

    Initial commit

---
+++ src/test/kotlin/jp/ac/it_college/std/s21018/bookmanager/news_manager/NewsReleaseManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.ac.it_college.std.s21018.bookmanager.news_manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewsReleaseManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
